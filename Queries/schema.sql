7f6a1ca042a341386b90565f6549884cb964a83e
William Davis <greg.davis55@gmail.com>
2020-12-27 12:27:22 -0600

    commit sql schema

---
+++ Queries/schema.sql
@@ -0,0 +1,28 @@
+-- Creating tables for PH-William Davis
+CREATE TABLE departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+-- Creating tables for PH-William Davis
+CREATE TABLE employees (
+     emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+-- Creating additional tables for PH-William Davis 
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+-- Select from departments 
+SELECT * FROM departments;
