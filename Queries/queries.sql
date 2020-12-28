e3b3622f0d45db4e2ad8418077702caa0d6d1a00
William Davis <greg.davis55@gmail.com>
2020-12-27 23:58:06 -0600

    commit queries and schema

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,18 +1,161 @@
--- Retirement eligibility
+-- Determine Retirement eligibility
 SELECT first_name, last_name
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
--- count number of employees Number of employees retiring
+
+-- Determine/Count number of employees retiring
 SELECT COUNT(first_name)
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
 -- create a new table 
 SELECT first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
 -- query new table 
 SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables (Shorten Code)
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_dateo_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp tables (Shorten Code)
+SELECT ri.emp_no,
+	ri.first_name,
+ri.last_name,
+	de.to_date 
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;a
+
+-- Left Join for retirement _info an dept_emp tables to verify still employed
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+
+-- Employee count by department number w/'ordering'
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Create new table 
+-- Determine the employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+Into employee_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+-- Select statement to check "to_date column"
+SELECT emp_no, first_name, last_name, gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create list of Employee information (shorten)
+SELECT e.emp_no,
+	e.first_name,
+e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+     AND (de.to_date = '9999-01-01');
+
+-- Create list of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+-- Create List of Department Retirees 
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name	
+Into dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+--employee count for sales team
+select ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	de.dept_no,
+	dpt.dept_name
+from current_emp as ce
+	inner join dept_emp as de
+		on (ce.emp_no = de.emp_no)
+	inner join departments as dpt
+		on (de.dept_no = dpt.dept_no)
+where dept_name = 'Sales';
+
+-- Employee counts for sales and Development Teams 
+select ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	de.dept_no,
+	dpt.dept_name
+from current_emp as ce
+	inner join dept_emp as de
+		on (ce.emp_no = de.emp_no)
+	inner join departments as dpt
+		on (de.dept_no = dpt.dept_no)
+where dept_name in ('Sales', 'Development');     
--- Queries/schema.sql
+++ Queries/schema.sql
@@ -26,3 +26,53 @@ CREATE TABLE salaries (
 );
 -- Select from departments 
 SELECT * FROM departments;
+-- Creating additional tables for PH-William Davis 
+CREATE TABLE dept_emp (
+emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+-- Creating Additional Tables 
+CREATE TABLE Titles (
+emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no)
+);
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+-- Joining retirement_info and dept_emp tables (Shorten Code)
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_dateo_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+-- Joining retirement_info and dept_emp tables (Shorten Code)
+SELECT ri.emp_no,
+	ri.first_name,
+ri.last_name,
+	de.to_date 
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;a
